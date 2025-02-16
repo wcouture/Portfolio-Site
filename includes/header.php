edd89cbf4221b2ccb437ed9a4eb307a0e41af6f0
Wiliam Couture <wpc21@fsu.edu>
2025-02-16 16:01:42 -0500

    Adds favicon to site

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="css/header.css">
         <link rel="stylesheet" type="text/css" href="games/innoculation/TemplateData/style.css">
+        <link rel="icon" type="image/x-icon" href="images/favicon.png">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
