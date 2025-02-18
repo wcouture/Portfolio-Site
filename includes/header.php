ad94cfe1fc8dfdaec9de27588ec6f6ca8aeced30
Wiliam Couture <wpc21@fsu.edu>
2025-02-18 16:13:10 -0500

    Sets website title

--- includes/header.php
+++ includes/header.php
@@ -15,6 +15,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+        <title>Will C. - Dev</title>
         <?php
         if (isset($page_css))
             echo $page_css;
