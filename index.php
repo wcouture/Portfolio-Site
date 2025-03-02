64776a7d89abbd02bc96d3b5c9f8b680f7ccbd14
Wiliam Couture <wpc21@fsu.edu>
2025-03-01 20:38:11 -0500

    Adds desc flag to sql query

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     include $root . "/includes/db-connect.php";
 
     $db = create_db_connection('portfolio');
-    $sql = "SELECT * FROM portfolio_item ORDER BY id;";
+    $sql = "SELECT * FROM portfolio_item ORDER BY id DESC;";
     $results = $db->query($sql);
     
     if ($results->num_rows <= 0) {
