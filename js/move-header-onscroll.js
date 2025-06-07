4e3ea2ced4a0ef5f9839c18ca41bd2f0e6a23494
William Couture <wcouture17@gmail.com>
2025-06-07 11:13:21 -0400

    Adjusts scroll limit to slide nav menu to side panel

--- js/move-header-onscroll.js
+++ js/move-header-onscroll.js
@@ -1,14 +1,17 @@
 var header = document.getElementById("header-nav-container");
-let pixel_limit = 80;
-    
-window.onscroll = function() {
-    handle_scroll();
+let pixel_limit = 40;
+
+window.onscroll = function () {
+  handle_scroll();
 };
 
 function handle_scroll() {
-    if (document.body.scrollTop > pixel_limit || document.documentElement.scrollTop > pixel_limit) {
-        header.className = "header-nav-container scrolled";
-    } else {
-        header.className = "header-nav-container";
-    }
+  if (
+    document.body.scrollTop > pixel_limit ||
+    document.documentElement.scrollTop > pixel_limit
+  ) {
+    header.className = "header-nav-container scrolled";
+  } else {
+    header.className = "header-nav-container";
+  }
 }
