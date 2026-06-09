8c5c793f6cd12dda8f6eefb0a8ce188617a60a8a
Will <wcouture17@gmail.com>
2026-06-09 14:55:22 -0400

    Update about page and update resume

--- about.php
+++ about.php
@@ -14,17 +14,17 @@ include_once $root . "/includes/header.php"
                 William Couture
             </div>
             <div class="about-subtitle">
-                Web Developer | IT Specialist
+                Full Stack Developer | IT Specialist
             </div>
             <div class="about-bio">
-            Graduating with a bachelor's of science in Computer Science and Cum Laude latin honors at Florida State University, programming has been a passion of mine since the beginning developing small video games in C# using the Unity game enigne.
+            Graduating with a bachelor's of science in Computer Science and Cum Laude latin honors at Florida State University, programming has been a passion of mine since the beginning. I began my journey developing mini games with C# utilizing the Unity game engine.
             <br><br>
-            More recently, I have branched into web development, designing a modern responsive website from the ground up utilizing NodeJS, and now with my current position as a PHP/WordPress developer and web technician at <a href="https://wfsu.org" target="_blank">WFSU Public Media</a>. My relentless drive for improvement and passion for programming has motivated me to develop various side projects to continually grow my skillset. The projects focus on data management, app development, education, and game development. 
+            Since then, I have pursued a career in web development, building and managing websites utilizing NodeJS, PHP, and C# with the .NET framework and Blazor. Throughout my career, I have been responsible for the full stack development of several websites, including the design and implementation of databases, APIs, and user interfaces. This has allowed me to develop a strong understanding of the entire web development process and the ability to work on all aspects of a project. I have also gained experience in IT support, providing technical assistance to users and troubleshooting hardware and software issues.
             <br><br>
-            This site aims to highlight several of my recent finished projects.
+            Application development, and programming as a whole brings me great joy. This site aims to showcase some of the projects I have chosen to work on in my free time, and give insight into my experience and background. If you have any questions or would like to connect, please feel free to reach out to me through the <a href="/contact.php">contact page</a> or click below to view my resume.
             </div>
             <div class="about-resume">
-                <a class="resume-link" href="/docs/william-couture-resume.pdf">
+                <a class="resume-link" href="/docs/WilliamCouture.pdf">
                     View Resume
                 </a>
             </div>
